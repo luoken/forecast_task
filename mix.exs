5681a8265cea080bd7917d5c03aa0bd6e99f9273
Ken <luozhirong@gmail.com>
2020-05-20 11:09:53 -0700

    adds in dialyxir

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Forecast.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_add_apps: [:mix]]
     ]
   end
 
@@ -22,7 +23,8 @@ defmodule Forecast.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.6"},
-      {:jason, "~> 1.2"}
+      {:jason, "~> 1.2"},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
